a211a2c6781e35ffda227f81dbbbe2bf26a25ec4
Faran <farandfx@gmail.com>
2019-11-18 11:23:00 +0700

    add comment feature

--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 const Comment = require ("../models/comment");
+const Socio = require ("../models/socio");
 
 //new form
 router.get("/socio/:id_post/comment/new",(req,res)=>{
@@ -11,20 +12,40 @@ router.get("/socio/:id_post/comment/new",(req,res)=>{
 });
 
 //create
-router.post("/socio/:id_post/comment",(req,res)=>{
-    let idPost = req.params.id_post;
-    Comment.create(req.params.comment, (err,result)=>{
+
+router.get("/socio/:idPost/comment", (req,res)=>{
+    let idPost = req.params.idPost;
+    let comment = req.body.comment;
+    
+    Socio.findById(idPost,(err,socioFound)=>{
         if(err){
             console.log(err);
-        } else{
-            res.redirect("/socio/" + idPost);
+        }else{
+            Comment.create(comment,(err,commentCreated)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    socioFound.comment.push(commentCreated);
+                    socioFound.save((err,result)=>{
+                        if(err){
+                            console.log(err)
+                        }else{
+                            res.redirect("/socio/" + idPost);
+                        }
+                    })
+                }
+            })
         }
-    });
-});
+    })
+})
 
 //edit form
 router.get("/socio/:id_post/comment/:id_comment", (req,res)=>{
     res.render("comment/edit");
 });
+
+//update
+
+//delete
 
 module.exports = router;
